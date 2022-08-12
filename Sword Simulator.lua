8ce7eb130ad97915f72efbccced4a5236aae89d8
SeaLion69 <103544448+SeaLion69@users.noreply.github.com>
2022-08-12 23:28:36 +0700

    Update Sword Simulator.lua

--- Sword Simulator.lua
+++ Sword Simulator.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("DNA (Sword Simulator)", "Midnight")
+local Window = Library.CreateLib("DNA HUB (Sword Simulator)", "Midnight")
 
 
 
